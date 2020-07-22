5d6d2fb6f19da8b3614f4e7067abeb88ca20f0ae
System Administrator <root@caoyanlingdeMacBook-Pro.local>
2020-07-22 10:25:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import { Button} from 'antd-mobile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './app.scss';
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      phoneVal:"",
+      codeVal:""
+    };
+    this.phoneNumEvent = this.phoneNumEvent.bind(this);
+    this.loginEvent = this.loginEvent.bind(this);
+  }
+  phoneNumEvent(){
+    alert(2222)
+  }
+  loginEvent(){
+    alert(1111)
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="cont">
+          <header>
+            <h1>欢迎登录,</h1>
+            <h1>社会治理融媒云投稿平台</h1>
+            <ul>
+              <li>
+                <input placeholder="请输入手机号码" value={this.state.phoneVal} onInput={this.phoneNumEvent} className="phoneNum"/>
+              </li>
+              <li>
+              <input placeholder="请输入验证码" value={this.state.codeVal} className="loginCode"/>
+              </li>
+            </ul>
+            <Button type="primary" className="login" onClick={this.loginEvent}>登录</Button>
+          </header>
+        </div>
+        
+      </div>
+    )
+  }
+  
 }
 
 export default App;

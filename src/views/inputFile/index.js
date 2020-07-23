870a0c27b6762a8189ea5aa3702ff27818083e7f
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-07-23 14:21:47 +0800

    投稿页面

--- src/views/inputFile/index.js
+++ src/views/inputFile/index.js
@@ -1,12 +1,40 @@
 import React,{Component} from 'react';
+import { NavBar,List} from 'antd-mobile';
 
+import './style.css';
+
+const Item = List.Item;
 
 class InputFile extends Component {
   
   render() {
     return (
       <div className="inputFile">
-            社会治理融媒云投稿平台
+            <NavBar
+              mode="light"
+              onLeftClick={() => console.log('onLeftClick')}
+              rightContent={[
+                <span className="iconfont icongerensucai"></span>
+              ]}
+            >社会治理融媒云投稿平台</NavBar>
+            <div className="content">
+              <List className="my-list">
+                <Item extra="请选择" arrow="horizontal" onClick={() => {}}>选择时间</Item>
+                <Item extra="请选择" arrow="horizontal" onClick={() => {}}>投稿状态</Item>
+              </List>
+
+              <List className="my-list">
+                <Item extra={'1231篇'}>累计发稿</Item>
+                <Item extra={'121篇'}>累计录用</Item>
+              </List>
+
+              <List className="my-list">
+                <Item wrap className="hadInputFile">今天何处春意浓？朋友圈堪比“赏春大赛”！你参与了吗？今天何处春意浓？今天何处春意浓？今天何处春意浓？朋友圈堪比“赏春今天何处春意浓？朋友圈堪比“赏春大赛”！你参与了吗？今天何处春意浓？今天何处春意浓？今天何处春意浓？</Item>
+              </List>
+
+            </div>
+          
+
       </div>
     )
   }
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -35,17 +35,17 @@ class Login extends Component {
     };
     var reg = 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/;
     var reg1 = /^\d{4}$/;
-    if (this.state.phoneVal == "") {
+    if (this.state.phoneVal === "") {
       Toast.fail('手机号不能为空', 3, null, false);
     } else if (!reg.test(this.state.phoneVal)) {
       Toast.fail('手机号格式不正确', 3, null, false);
-    } else if (this.state.codeVal == "") {
+    } else if (this.state.codeVal === "") {
       Toast.fail('验证码不能为空', 3, null, false);
     } else if (!reg1.test(this.state.codeVal)) {
       Toast.fail('验证码格式不正确', 3, null, false);
     } else {
       Api.getCodeLogin(params).then(res => {
-        if (res.resp_code == 0) {
+        if (res.resp_code === 0) {
           localStorage.setItem("access_token", res.datas.access_token);
           localStorage.setItem("token_type", res.datas.token_type);
           localStorage.setItem("globalAccount", res.datas.globalAccount);

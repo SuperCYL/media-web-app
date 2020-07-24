df0d7cf8fb4ec8a05d01ad597070df3790c988fc
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-07-24 16:48:39 +0800

    创建稿件

--- src/views/createFile/index.js
+++ src/views/createFile/index.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react'
-import {  } from 'antd-mobile';
+import { List ,Picker} from 'antd-mobile';
 import './style.css'
+
+const Item = List.Item;
 export class CreateFile extends Component {
     constructor(props){
         super(props);
         this.state={
-
+            fileType:[],
+            fileTypeList:[
+                {
+                    lable:"",value:""
+                }
+            ]
         }
+    }
+    //确定->稿件类型
+    fileTypeOk=(v)=>{
+        this.setState({fileType:v})
+    }
+    //取消->稿件类型
+    fileTypeDis(){
+        this.setState({fileType:[]})
+    }
+    createFileBack(){
+        this.props.history.push('/InputFile');
     }
     render() {
         return (
             <div className="createFile">
-                <div className="header"><span className='iconfont iconarrow-left-bold' />新建稿件</div>
+                <div className="header"><span className='iconfont iconarrow-left-bold' onClick={this.createFileBack.bind(this)}/>新建稿件</div>
+                <div className="content">
+                    <List className="my-list">
+                        <Picker 
+                        data={this.state.fileTypeList}
+                        cols={1}
+                        value={this.state.fileType}
+                        onOk={this.fileTypeOk.bind(this)}
+                        onDismiss={this.fileTypeDis.bind(this)}
+                        >
+                        <Item extra="请选择" arrow="horizontal">投稿类型</Item>
+                        </Picker>
+                    </List>
+                </div>
             </div>
         )
     }
